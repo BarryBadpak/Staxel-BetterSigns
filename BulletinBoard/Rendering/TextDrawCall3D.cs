ea0d37258bd84be2feeb6b6c35fdd5717ff5725c
Stefan Haneveer <s.haneveer93@gmail.com>
2018-03-04 01:36:34 +0100

    Initial working version of world text renderer with a basic bitmap font implementation

---
+++ BulletinBoard/Rendering/TextDrawCall3D.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Plukit.Base;
+
+namespace BulletinBoard.Rendering
+{
+	public struct TextDrawCall3D
+	{
+		public readonly string Message;
+		public readonly Vector3D Location;
+		public readonly uint rotation;
+
+		public TextDrawCall3D(string message, Vector3D location, uint rotation)
+		{
+			this.Message = message;
+			this.Location = location;
+			this.rotation = rotation;
+		}
+	}
+}
---
+++ BulletinBoard/TileState/BulletinBoardTileStateEntityPainter.cs
@@ -0,0 +1,69 @@
+﻿using BulletinBoard.Rendering;
+using Plukit.Base;
+using Staxel.Client;
+using Staxel.Draw;
+using Staxel.Logic;
+using Staxel.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletinBoard.TileState
+{
+	class BulletinBoardTileStateEntityPainter : EntityPainter
+	{ 
+		private WorldTextRenderer WorldTextRenderer = new WorldTextRenderer();
+		private BillboardNumberRenderer BillboardRenderer = new BillboardNumberRenderer();
+		private bool _initialised = false;
+		private Vector3D _position;
+		private uint _rotation;
+
+		/// <summary>
+		/// Clear out any old pending draw calls pre-render
+		/// </summary>
+		/// <param name="graphics"></param>
+		/// <param name="renderOrigin"></param>
+		/// <param name="entity"></param>
+		/// <param name="avatarController"></param>
+		/// <param name="renderTimestep"></param>
+		public override void BeforeRender(DeviceContext graphics, Vector3D renderOrigin, Entity entity, AvatarController avatarController, Timestep renderTimestep) {
+			//this.WorldTextRenderer.Purge();
+		}
+
+		public override void Render(DeviceContext graphics, Matrix4F matrix, Vector3D renderOrigin, Entity entity, AvatarController avatarController, Timestep renderTimestep, RenderMode renderMode)
+		{
+			if (renderMode == RenderMode.Normal)
+			{
+				this.WorldTextRenderer.Draw(graphics, renderOrigin, matrix);
+			}
+		}
+
+		public override void RenderUpdate(Timestep timestep, Entity entity, AvatarController avatarController, EntityUniverseFacade facade, int updateSteps) {}
+
+		public override void ClientPostUpdate(Timestep timestep, Entity entity, AvatarController avatarController, EntityUniverseFacade facade) { }
+		public override void ClientUpdate(Timestep timestep, Entity entity, AvatarController avatarController, EntityUniverseFacade facade) {
+
+			if (!this._initialised)
+			{
+				BulletinBoardTileStateEntityLogic logic = entity.TileStateEntityLogic as BulletinBoardTileStateEntityLogic;
+				if (logic != null)
+				{
+					this._position = logic.GetInitialDrawPosition();
+
+					if (this._position != Vector3D.Zero)
+					{
+						this._rotation = (logic.GetVariation() >> 10) & 3;
+						this._initialised = true;
+
+						this.WorldTextRenderer.DrawString("De kat krabt de krullen van de trap.", this._position, this._rotation);
+					}
+				}
+			}
+		}
+
+		public override void StartEmote(Entity entity, Timestep renderTimestep, EmoteConfiguration emote) {}
+		protected override void Dispose(bool disposing) {}
+	}
+}
